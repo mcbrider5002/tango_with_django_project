e72618d23b959bfe7f8dce4b0c0f540d1097abf1
Ross McBride <2185647M@student.gla.ac.uk>
2017-01-26 15:32:05 +0000

    End of lab session

---
+++ populate_rango.py
@@ -0,0 +1,68 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
+						'tango_with_django_project.settings')
+						
+import django
+django.setup()
+
+from rango.models import Category, Page
+
+def populate():
+	#first uses lists of dictionaries containing the pages added to each category
+	#then a dictionary of dictionaries for the categories
+	
+	python_pages = [
+		{"title": "Official Python Tutorial",
+		 "url":"http://docs.python.org/2/tutorial/"},
+		{"title": "How to Think Like a Computer Scientist",
+		 "url":"http://www.greenteapress.com/thinkpython/"},
+		{"title": "Learn Python in Ten Minutes",
+		 "url":"http://www.korokithakis.net/tutorials/python/"}]
+		 
+	django_pages = [
+		{"title": "Official Django Tutorial",
+		 "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+		{"title": "Django Rocks",
+		 "url":"http://www.djangorocks.com/"},
+		{"title": "How to Tango with Django",
+		 "url":"http://www.tangowithdjango.com/"}]
+		 
+	other_pages = [
+		{"title": "Bottle",
+		 "url":"http://bottlepy.org/docs/dev/"},
+		{"title": "Flask",
+		 "url":"http://flask.pocoo.org"}]
+		 
+	cats = {"Python": {"pages": python_pages},
+			"Django": {"pages": django_pages},
+			"Other Frameworks": {"pages": other_pages}}
+			
+	#go through the cats dictionary, add each category
+	#then add all associated pages for that category
+	
+	for cat, cat_data in cats.iteritems():
+		c = add_cat(cat)
+		for p in cat_data["pages"]
+			add_page(c, p["title"], p["url"])
+			
+	#print all added categories
+	for c in Category.objects.all():
+		for p in Page.objects.filter(category=c):
+			print("- {0} - {1}".format(str(c), str(p)))
+			
+def add_page(cat, title, url, views=0):
+	p = Page.objects.get_or_create(category=cat, title = title)[0]
+	p.url = url
+	p.views = views
+	p.save()
+	return p
+	
+def_cat(name):
+	c = Category.objects.get_or_create(name = name)[0]
+	c.save()
+	return c
+	
+#Start execution here!
+if __name__ == '__main__':
+	print("Starting Rango population script...")
+	populate()
