76235cf8d3ed7813d0103d042d4a2d3a6f297b6f
Ross McBride <mcbrider5002@gmail.com>
2017-01-27 17:23:41 +0000

    Deadline, near end of Ch6

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-	#context dictionary to replace template variable names
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	#return a rendered response to return to client
-	#note second parameter is our template
+	#query db for all categories currently stored
+	#order by likes in descending order
+	#retrieve top 5 (or all if less than five)
+	#place list in context_dict to pass on to template engine
+	
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	
+	#render response and return
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
 	return render(request, 'rango/about.html')
 
+def show_category(request, category_name_slug):
+	context_dict = {}
+	
+	#returns a category with the given slug name
+	#if it does not exist, raises DoesNotExist exception
+	try:
+		category = Category.objects.get(slug=category_name)
 
+		#finds all associated pages matching given category
+		pages = Page.objects.filter(category=category)
+	
+		context_dict['pages'] = pages
+	
+		#adds category object for existence check
+		context_dict['category'] = category
+		
+	except Category.DoesNotExist:
+		#if the category can't be found
+		#let the template display no category message
+		context_dict['category'] = None
+		context_dict['pages'] = None
+		
+	#render response and return
+	return render(request, 'rango/category.html', context_dict)
