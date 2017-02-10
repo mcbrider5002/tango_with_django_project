5d5f28d5db587d4836a23c77b38cf5d7750dde2a
Ross McBride <mcbrider5002@gmail.com>
2017-02-10 13:19:10 +0000

    End of Chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -45,7 +45,7 @@ class UserForm(forms.ModelForm):
 		model = User
 		fields = ('username', 'email', 'password')
 		
-class UserProfileForm(forms.ModelForm)
+class UserProfileForm(forms.ModelForm):
 	class Meta:
 		model = UserProfile
 		fields = ('website', 'picture')
