5f4a28583bc63332976ae7c407a46c249e31d5ca
Ross McBride <mcbrider5002@gmail.com>
2017-02-10 10:45:51 +0000

    End of Chapter 8 + Some of Chapter 9 - Oops!

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 from django.db import models
 
@@ -39,3 +40,15 @@ class Page(models.Model):
 		
 	def __unicode__(self): 
 		return self.title
+		
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	
+	def __str__(self): 
+		return self.name
+		
+	def __unicode__(self): 
+		return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.forms import CategoryForm, PageForm
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 def index(request):
 	#query db for all categories currently stored
@@ -83,3 +83,33 @@ def add_page(request, category_name_slug):
 			print(form.errors)
 			
 	return render(request, 'rango/add_page.html', {'form': form, 'category': category})
+	
+def register(request):
+	registered=False
+	
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+		
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			
+			profile = profile_form.save(commit=False)
+			profile.user = user
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+			profile.save()
+			
+			registered = True
+		else:
+			print(user_form.errors, profile_form.errors)
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+		
+	return render(request, 'rango/register.html',
+					{'user_form': user_form,
+					 'profile_form': profile_form,
+					 'registered': registered})
